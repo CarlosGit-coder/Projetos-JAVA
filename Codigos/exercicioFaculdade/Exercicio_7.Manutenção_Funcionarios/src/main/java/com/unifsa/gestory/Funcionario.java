ce3aba0b1999e51a1a475292e2288f35ecf6c412
CarlosGit-coder <carlosgabrielmonteiro2006@gmail.com>
2026-03-31 12:47:38 -0300

    Adição dos metodos e a utilização do toString para exibir os dados, pronto para começaer a criação da classe assistente

--- Codigos/exercicioFaculdade/Exercicio_7.Manutenção_Funcionarios/src/main/java/com/unifsa/gestory/Funcionario.java
+++ Codigos/exercicioFaculdade/Exercicio_7.Manutenção_Funcionarios/src/main/java/com/unifsa/gestory/Funcionario.java
@@ -4,4 +4,21 @@ public class Funcionario {
     public String nome;
     public double salario;
 
+    public double addAumento(Funcionario f) {
+        f.salario +=  (f.salario * 20) / 100;
+        return f.salario;
+    }
+    public double ganhoAnual() {
+        return this.salario * 13;
+    }
+    public void exibirDados() {
+        System.out.println(this.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "Nome do Funcionário: " + this.nome +
+                "Salario: " + this.salario +
+                "Ganho anual: " + ganhoAnual();
+    }
 }

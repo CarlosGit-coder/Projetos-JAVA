d0e42ec59b08d712387792857d54064e4834a3cc
CarlosGit-coder <carlosgabrielmonteiro2006@gmail.com>
2026-04-06 21:21:30 -0300

    Criação dos metodos e atributos da classe Animal, pronto para começar a classe cachorro

---
+++ Codigos/exercicioFaculdade/exercicio_8.diagramas/src/main/java/com/unifsa/diagrama/Animal.java
@@ -0,0 +1,22 @@
+package com.unifsa.diagrama;
+
+public class Animal {
+    private String nome;
+    private String raca;
+
+    public Animal() {
+
+    }
+
+    public Animal(Animal a) {
+        this.nome = a.nome;
+    }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getRaca() { return raca; }
+    public void setRaca(String raca) { this.raca = raca; }
+
+    public String caminha() {
+        return nome + " está caminhando.";
+    }
+}
